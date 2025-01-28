42e2a47485981b391ac2dec4e0209b144f6ccee0
snype <snypeatwork@gmail.com>
2025-01-29 05:09:03 +0545

    autoload error

---
+++ Console/Commands/NPlusOneLogCommand.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Snype\Nplusone\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class NPlusOneLogCommand extends Command
+{
+    protected $signature = 'nplusone:log';
+    protected $description = 'Display N+1 query logs';
+
+    public function handle()
+    {
+        $logs = Log::channel('daily')->getLogs();
+        $this->info("N+1 Query Logs:");
+        foreach ($logs as $log) {
+            $this->line($log);
+        }
+    }
+}
